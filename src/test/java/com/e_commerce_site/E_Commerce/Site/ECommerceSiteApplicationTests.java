ca18a066a6de8a88592759b482ce4853c9861990
Nilesh <nsmeshram29@gmail.com>
2025-04-19 03:05:22 +0530

    Intialize the project and setup on vs code and github

---
+++ src/test/java/com/e_commerce_site/E_Commerce/Site/ECommerceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce_site.E_Commerce.Site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
